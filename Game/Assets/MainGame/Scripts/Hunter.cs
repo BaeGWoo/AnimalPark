e566f26de6edcd0fc9ff4f5e4d006310b6f0202a
BaeGWoo <gwbae4211@naver.com>
2024-12-13 02:23:12 +0900

    add Asset

--- Game/Assets/MainGame/Scripts/Hunter.cs
+++ Game/Assets/MainGame/Scripts/Hunter.cs
@@ -1,10 +1,8 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using static UnityEngine.GraphicsBuffer;
+
 
 public class Hunter : MonoBehaviour
 {
